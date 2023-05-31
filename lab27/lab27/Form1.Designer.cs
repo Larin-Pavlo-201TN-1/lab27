521c0ec1757634edaddb151c2fc361d569eb618b
Larin-Pavlo-201TN-1 <98223565+Larin-Pavlo-201TN-1@users.noreply.github.com>
2023-05-31 18:46:08 +0300

    Add files via upload

---
+++ lab27/lab27/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace lab27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Atributes_txt = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(408, 185);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Відобразити\r\nпапки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Властивості\r\nдиску/папки/файлу\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Фільтрування файлів";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Атрибути файлу або папки:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(136, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Відкрити текстовий файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Відкрити зображення";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(297, 260);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 42);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Атрибути безпеки";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Atributes_txt
+            // 
+            this.Atributes_txt.AutoSize = true;
+            this.Atributes_txt.Location = new System.Drawing.Point(206, 321);
+            this.Atributes_txt.Name = "Atributes_txt";
+            this.Atributes_txt.Size = new System.Drawing.Size(0, 16);
+            this.Atributes_txt.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 555);
+            this.Controls.Add(this.Atributes_txt);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label Atributes_txt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ lab27/lab27/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace lab27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode node = new TreeNode(drive.Name);
+                node.Tag = drive.RootDirectory.FullName;
+                node.Nodes.Add(new TreeNode());
+                treeView1.Nodes.Add(node);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            if (node.Nodes.Count == 1 && node.Nodes[0].Tag == null)
+            {
+                node.Nodes.Clear();
+
+                string path = node.Tag.ToString();
+                DirectoryInfo directory = new DirectoryInfo(path);
+                try
+                {
+                    foreach (DirectoryInfo subdirectory in directory.GetDirectories())
+                    {
+                        TreeNode subnode = new TreeNode(subdirectory.Name);
+                        subnode.Tag = subdirectory.FullName;
+                        subnode.Nodes.Add(new TreeNode());
+                        node.Nodes.Add(subnode);
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Доступ обмежений.");
+                }
+
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    TreeNode subnode = new TreeNode(file.Name);
+                    subnode.Tag = file.FullName;
+                    node.Nodes.Add(subnode);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            string path = node.Tag.ToString();
+            DirectoryInfo directory = new DirectoryInfo(path);
+            proper proper = new proper();
+            proper.direc = directory;
+            proper.ShowDialog();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            string path = node.Tag.ToString();
+            DirectoryInfo directory = new DirectoryInfo(path);
+            Atributes_txt.Text = directory.Attributes.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            string path = node.Tag.ToString();
+            DirectoryInfo directory = new DirectoryInfo(path);
+            if(directory.Extension == ".txt")
+            {
+                try
+                {
+                    Process.Start("notepad.exe", path);
+                }
+                catch
+                {
+                    MessageBox.Show("Упс! щось пішло не так.");
+                }
+            }
+            else
+                MessageBox.Show("Потрібне ррозширення: txt");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            TreeNode node = treeView1.SelectedNode;
+            string path = node.Tag.ToString();
+            DirectoryInfo directory = new DirectoryInfo(path);
+            if (directory.Extension == ".png" || directory.Extension == ".jpg")
+            {
+                try
+                {
+                    Process.Start("iexplore.exe", path);
+                }
+                catch
+                {
+                    MessageBox.Show("Упс! щось пішло не так.");
+                }
+            }
+            else
+                MessageBox.Show("Потрібне розширення: jpg та png");
+        }
+    }
+
+}
